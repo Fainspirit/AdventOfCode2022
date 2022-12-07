e0c2063e540c96f14efc91124ad1b2a2e7ef1a99
Fainspirit <31144778+Fainspirit@users.noreply.github.com>
2022-12-06 22:26:15 -0600

    Added Day template

---
+++ AdventOfCode2022/DayN.cs
@@ -0,0 +1,17 @@
+namespace AdventOfCode2022
+{
+    internal class DayN : Day
+    {
+       
+        public override void Run()
+        {
+            string[] lines = input.Split("\r\n");
+            int answer = 0;
+            
+            Console.WriteLine("The answer is {0}", answer);
+        }
+
+        // Copy / paste input string in the quotes
+        string input = "";
+    }
+}
