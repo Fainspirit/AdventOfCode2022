6e6c8dd75437e9d0c99338b9c94c00dc3060c919
Fainspirit <31144778+Fainspirit@users.noreply.github.com>
2022-12-08 21:54:50 -0600

    Updated template to read from files now

--- AdventOfCode2022/DayN.cs
+++ AdventOfCode2022/DayN.cs
@@ -2,16 +2,25 @@ namespace AdventOfCode2022
 {
     internal class DayN : Day
     {
-       
+        // Place a file in input subfolder
+        readonly string SOURCE_DIR = "../../../input/";
+
         public override void Run()
         {
-            string[] lines = input.Split("\r\n");
+            string inputPath = "day8.txt";
+            string[] lines = ReadInput(SOURCE_DIR + inputPath);
             int answer = 0;
-            
+
+            // Code goes here
+
+
+
             Console.WriteLine("The answer is {0}", answer);
         }
 
-        // Copy / paste input string in the quotes
-        string input = "";
+        static string[] ReadInput(string path)
+        {
+            return File.ReadAllLines(path);
+        }
     }
 }
